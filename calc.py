9f37734749d18613da4cad8ef5d6c890c97bddb3
steb24 <steven.malunes24@gmail.com>
2026-08-04 21:01:44 +0800

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,29 @@
+while True:
+    operator = input("Enter operator: ")
+    if operator == "+":
+        break
+    elif operator == "-":
+        break
+    elif operator == "*":
+        break
+    elif operator == "/":
+            break
+    else:
+        print("Invalid input. Please type a valid operator")
+first = float(input("Enter 1st number: "))
+second = float(input("Enter 2nd number: "))
+
+if operator == "+":
+    print(first + second)
+elif operator == "-":
+    print(first - second)
+elif operator == "*":
+    print(first * second)
+elif operator == "/":
+    print(first / second)
+
+
+
+
+
+
